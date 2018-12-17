f9d1695a9e101d4182890f5f01095c11bc9a943d
Tony5t4rk <978539910@qq.com>
2018-12-17 18:30:23 +0800

    Strive! ! !

--- AC代码/AYITOJ/AYITOJ-44.cpp
+++ AC代码/AYITOJ/AYITOJ-44.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 0x3f3f3f3f3f3f3f3f;
 const int maxn = 2e5 + 5;
 
 struct Edge {
     long long V, Dis, Next;
 };
 
-Edge edges[maxn << 7];
+Edge edges[maxn << 4];
 int Head[maxn << 1];
 int Tot;
 
@@ -30,13 +29,16 @@ struct Cmp {
     }
 };
 
-void Dijkstra(int Start) {
+long long Dijkstra(int Start, int End) {
     priority_queue<int, vector<int>, Cmp> Que;
-    memset(Dis, INF, sizeof(Dis));
+    memset(Dis, 0x3f, sizeof(Dis));
     Dis[Start] = 0;
     Que.push(Start);
     while (!Que.empty()) {
         int Cur = Que.top(); Que.pop();
+        if (Cur == End) {
+            return Dis[End];
+        }
         for (int i = Head[Cur]; ~i; i = edges[i].Next) {
             if (Dis[edges[i].V] > Dis[Cur] + edges[i].Dis) {
                 Dis[edges[i].V] = Dis[Cur] + edges[i].Dis;
@@ -44,35 +46,37 @@ void Dijkstra(int Start) {
             }
         }
     }
+    return -1 * 1LL;
 }
 
 long long N, S, T;
 long long U[maxn], V[maxn];
-vector<long long> numbers;
+long long numbers[maxn << 1];
+long long Cnt, Size;
 
 int Disperse(long long X) {
-    return lower_bound(numbers.begin(), numbers.end(), X) - numbers.begin() + 1;
+    return lower_bound(numbers, numbers + Size, X) - numbers + 1;
 }
 
 int main(int argc, char *argv[]) {
     Init();
     scanf("%lld%lld%lld", &N, &S, &T);
-    numbers.push_back(S); numbers.push_back(T);
+    Cnt = 0;
+    numbers[Cnt++] = S; numbers[Cnt++] = T;
     for (long long i = 1; i <= N; ++i) {
         scanf("%lld%lld", &U[i], &V[i]);
-        numbers.push_back(U[i]); numbers.push_back(V[i]);
+        numbers[Cnt++] = U[i]; numbers[Cnt++] = V[i];
     }
-    sort(numbers.begin(), numbers.end());
-    numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
-    for (int i = 0; i < int(numbers.size()) - 1; ++i) {
+    sort(numbers, numbers + Cnt);
+    Size = unique(numbers, numbers + Cnt) - numbers;
+    for (int i = 0; i < Size - 1; ++i) {
         AddEdge(i + 1, i + 2, numbers[i + 1] - numbers[i]);
         AddEdge(i + 2, i + 1, numbers[i + 1] - numbers[i]);
     }
     for (int i = 1; i <= N; ++i) {
         AddEdge(Disperse(U[i]), Disperse(V[i]), 1);
     }
-    Dijkstra(Disperse(S));
-    printf("%lld\n", Dis[Disperse(T)]);
+    printf("%lld\n", Dijkstra(Disperse(S), Disperse(T)));
     return 0;
 }
 
---
+++ AC代码/CodeForces-GYM/CodeForces-GYM102028-E.py
@@ -0,0 +1,34 @@
+import math
+
+maxn = 1005
+
+def PrimeInit():
+    IsPrime = [True] * maxn
+    Prime = []
+    for i in range(2, maxn):
+        if IsPrime[i]:
+            Prime.append(i)
+            for j in range(i * i, maxn, i):
+                IsPrime[j] = False;
+    return Prime
+
+def main():
+    Prime = PrimeInit()
+    T = int(input())
+    for Case in range(T):
+        N = int(input())
+        Book, Up, Down = 1, 1, 1
+        for i in Prime:
+            Book *= i
+            if Book > N:
+                break
+            Up *= i
+            Down *= i + 1
+        Gcd = math.gcd(Up, Down)
+        Up //= Gcd
+        Down //= Gcd
+        print(str(Up) + '/' + str(Down))
+
+if __name__ == '__main__':
+    main()
+
