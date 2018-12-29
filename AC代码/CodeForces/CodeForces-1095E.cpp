c5d74eb761784db998af624faf8c8aae34ef432d
Tony5t4rk <978539910@qq.com>
2018-12-29 19:17:24 +0800

    Strive! ! !

---
+++ AC代码/CodeForces/CodeForces-1095E.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 1e6 + 5;
+
+int N;
+char Str[maxn];
+int Prefix[maxn];
+int Suffix[maxn];
+bool PreFlag[maxn], SufFlag[maxn];
+int Ans;
+
+int main(int argc, char *argv[]) {
+    scanf("%d%s", &N, Str + 1);
+    PreFlag[0] = true;
+    for (int i = 1; i <= N; ++i) {
+        Prefix[i] = Prefix[i - 1] + (Str[i] == '(' ? 1 : -1);
+        PreFlag[i] = PreFlag[i - 1] & (Prefix[i] >= 0);
+    }
+    SufFlag[N + 1] = true;
+    for (int i = N; i >= 1; --i) {
+        Suffix[i] = Suffix[i + 1] + (Str[i] == ')' ? 1 : -1);
+        SufFlag[i] = SufFlag[i + 1] & (Suffix[i] >= 0);
+    }
+    for (int i = 1; i <= N; ++i) {
+        if (!PreFlag[i - 1] || !SufFlag[i + 1]) {
+            continue;
+        }
+        if (Str[i] == '(') {
+            if (Prefix[i - 1] > 0 && Prefix[i - 1] - 1 - Suffix[i + 1] == 0) {
+                ++Ans;
+            }
+        }
+        else {
+            if (Prefix[i - 1] + 1 - Suffix[i + 1] == 0) {
+                ++Ans;
+            }
+        }
+    }
+    printf("%d\n", Ans);
+    return 0;
+}
+
