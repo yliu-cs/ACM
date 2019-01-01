a8c520ab3eeb41e4912596d94767912123ca4677
Tony5t4rk <978539910@qq.com>
2019-01-01 14:45:21 +0800

    Strive! ! !

---
+++ AC代码/HenuOJ/HenuOJ-1060.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string A, B;
+
+int main(int argc, char *argv[]) {
+    while (cin >> A >> B) {
+        if (A == B) {
+            printf("ali\n");
+        }
+        else {
+            printf("taoz1\n");
+        }
+    }
+    return 0;
+}
+
---
+++ AC代码/HenuOJ/HenuOJ-1075.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long N;
+
+int main(int argc, char *argv[]) {
+    scanf("%lld", &N);
+    printf("%lld\n", N * (N - 1));
+    return 0;
+}
+
