5f21fcf12c7b520f3116d122d471bb7b4b7f7ffc
Tony5t4rk <33507360+Tony5t4rk@users.noreply.github.com>
2019-01-08 13:06:09 +0800

    Update

--- Push.py
+++ Push.py
@@ -6,15 +6,15 @@ README = open('README.md', 'w')
 
 def Work():
     global AllCnt
-    README.write('# Count of Solved Problems  \n')
+    README.write('# Count of Solved Problems  \n```\n')
     Index = os.getcwd() + '/ACCode'
     OJs = os.listdir(Index)
     OJs.sort()
     for OJ in OJs:
         Cnt = len(os.listdir(Index + '/' + OJ))
         AllCnt += Cnt
-        README.write('{:<30s}{:d}{:s}'.format(OJ, Cnt, '  \n'))
-    README.write('#### `Total:' + str(AllCnt) + '`\n')
+        README.write('{:<25s}{:d}{:s}'.format(OJ, Cnt, '  \n'))
+    README.write('```\n#### `Total:' + str(AllCnt) + '`\n')
 
 def main():
     Work()
