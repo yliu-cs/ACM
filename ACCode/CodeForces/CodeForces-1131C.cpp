c29c610e427b0f02a626a57089a7a4c59f4fd278
Tony5t4rk <978539910@qq.com>
2019-02-25 21:33:04 +0800

    Update

--- ACCode/CodeForces/CodeForces-1131C.cpp
+++ ACCode/CodeForces/CodeForces-1131C.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
 int main(int argc, char *argv[]) {
     int n; cin >> n;
-    vector<ll> a(n);
+    vector<int> a(n);
     for (auto &it : a) cin >> it;
     sort(a.begin(), a.end());
-    for (int i = 0; i < a.size(); i += 2) cout << a[i] << " ";
-    for (int i = (a.size() & 1 ? (a.size() - 2) : (a.size() - 1)); i >= 0; i -= 2) cout << a[i] << " ";
+    reverse(a.begin(), a.end());
+    deque<int> Deq;
+    bool Flag = true;
+    for (auto &it : a) {
+        if (Flag) Deq.push_back(it);
+        else Deq.push_front(it);
+        Flag = !Flag;
+    }
+    for (auto &it : Deq) cout << it << " ";
     return 0;
 }
 
