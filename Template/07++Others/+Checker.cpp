584836344e7f0049cac35c6771f5e476ed4228b4
Tony5t4rk <978539910@qq.com>
2019-05-16 19:28:30 +0800

    Update

--- Template/07++Others/+Checker.cpp
+++ Template/07++Others/+Checker.cpp
@@ -8,5 +8,21 @@
   if not errorlevel 1 goto loop
   pause
   :end
+
+  // Linux
+  declare -i n=1
+  while (true)
+    do
+    ./dtmk
+    ./my < 1.in > my.out
+    ./force < 1.in > for.out
+    if diff my.out for.out
+    then
+      echo right $n
+      n=n+1
+    else
+      exit
+    fi
+  done
 */
 
